ea3cd1ba785fc316de5201e2a56166fdbee493ff
Chris <nwodochristian@gmail.com>
2019-09-17 09:19:30 +0100

    babel webpack and eslint setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './build/bundle.js',
+  entry: './src/index.js',
   output: {
     path: __dirname + '/build',
     publicPath: '/',
